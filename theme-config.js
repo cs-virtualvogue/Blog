311cad401d3fe9614b3228866f7db3ae27009f97
cs-virtualvogue <cs.virtualvogue@gmail.com>
2026-08-11 14:07:31 +0700

    Add files via upload

---
+++ theme-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Prompt', 'Inter', 'sans-serif'],
+                serif: ['Georgia', 'serif'],
+            },
+            colors: {
+                sand: {
+                    50: '#FAF9F6',
+                    100: '#F4F1EA',
+                    200: '#E7DFD5',
+                    300: '#D5C7B5',
+                    800: '#52493E',
+                    900: '#2E2720',
+                },
+                stone: {
+                    850: '#21201D',
+                }
+            }
+        }
+    }
+}
